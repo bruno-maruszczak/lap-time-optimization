5ec3a411982e19043ea69a5131ae6d8c99f6c96e
bruno-maruszczak <72678261+bruno-maruszczak@users.noreply.github.com>
2024-05-24 13:44:23 +0200

    Modified shell.nix to use nix-ld Use it to load required dynamic libraries on NixOS

--- shell.nix
+++ shell.nix
@@ -6,17 +6,19 @@ mkShell {
     (python311.withPackages (pp: with pp; [
             pip
           ]))
-
+    texliveBasic
+    texlivePackages.type1cm
+    texlivePackages.dvipng
   ];
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
 
   NIX_LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
     stdenv.cc.cc
-    zlib
   ];
 
 
   shellHook = ''
+    export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
     if [ ! -d "./venv" ]; then
       python -m venv ./venv
     fi
