535b3a2ee4d69198e589ef49e1c53ea306a95c56
Bruno Maruszczak <72678261+bruno-maruszczak@users.noreply.github.com>
2024-09-08 00:12:38 +0200

    Initial implementation of the new car model
    Squashed commit of the following:
    
    commit 19249caf999ddf67140c53d584b52d88134be540
    Author: Bruno Maruszczak <72678261+bruno-maruszczak@users.noreply.github.com>
    Date:   Sun Sep 8 00:02:15 2024 +0200
    
        Implemented model equations
    
    commit 40139e711329f281c2898195afa1f02dc7689c91
    Author: Bruno Maruszczak <72678261+bruno-maruszczak@users.noreply.github.com>
    Date:   Sat Sep 7 18:36:12 2024 +0200
    
        Scaffolded improved car model
    
    commit df03ee588621b623e5171cb9510be2353d831333
    Author: Bruno Maruszczak <72678261+bruno-maruszczak@users.noreply.github.com>
    Date:   Sat Sep 7 15:08:56 2024 +0200
    
        Added MPC lib requirements

---
+++ src/model.py
@@ -0,0 +1,102 @@
+import numpy as np
+import do_mpc
+from path import Path as Spline
+
+class VehicleModel:
+    def __init__(self, track_line: Spline):
+        self.track_line = track_line
+        self.mass = 1.0
+        self.rotational_inertia = 1.0
+        self.length_f = 1.0
+        self.B_f = 1.0
+        self.C_f = 1.0
+        self.D_f = 1.0
+        self.length_r = 1.0
+        self.B_r = 1.0
+        self.C_r = 1.0
+        self.D_r = 1.0
+        self.Cr_0 = 0.0
+        self.Cr_2 = 0.0
+        self.ptv = 0.0
+        self.model = self.create_model()
+
+    def load_params(self, path):
+        self.mass = 1.0
+        self.rotational_inertia = 1.0
+        self.length_f = 1.0
+        self.A_f = 1.0
+        self.B_f = 1.0
+        self.C_f = 1.0
+        self.length_r = 1.0
+        self.A_r = 1.0
+        self.B_r = 1.0
+        self.C_r = 1.0
+        self.Cr_0 = 0.0
+        self.Cr_2 = 0.0
+        self.ptv = 0.0
+
+
+    def k(self, s):
+        return self.track_line.find_curvature_at_s(s)
+
+    def create_model(self) -> do_mpc.model.Model:
+        """
+        Setups all variables, inputs, parameters of an MPC model.
+        """
+
+        model_type = 'continuous'
+        model = do_mpc.model.Model(model_type)
+
+        s = model.set_variable("_x", 's', shape=(1,1))
+        n = model.set_variable("_x", 'n', shape=(1,1))
+        mu = model.set_variable("_x", 'mu', shape=(1,1))
+
+        vx = model.set_variable('_x', 'vx', shape=(3,1))
+        vy = model.set_variable('_x', 'vy', shape=(3,1))
+        r = model.set_variable('_x', 'r', shape=(1,1))
+
+        steering_angle = model.set_variable('_x', 'steering_angle', shape=(1,1))
+        throttle = model.set_variable('_x', 'throttle', shape=(1,1))
+
+        steering_angle_change = model.set_variable('_u', 'steering_angle_change', shape=(1,1))
+        throttle_change = model.set_variable('_u', 'throttle_change', shape=(1,1))
+
+
+        sdot = (vx*np.cos(mu) - vy*np.sin(mu)) / (1 - n * self.k(s))
+        
+        # Tires
+        alpha_f = np.arctan((vy + self.length_f*r)/vx) - steering_angle
+        alpha_r = np.arctan((vy - self.length_r*r)/vx)
+        g = 9.81
+        Fn_f = self.length_r * self.mass * g / (self.length_f + self.length_r)
+        Fn_r = self.length_f * self.mass * g / (self.length_f + self.length_r)
+        Fy_f = Fn_f * self.D_f * np.sin(self.C_f * np.arctan(self.B_f * alpha_f))
+        Fy_r = Fn_r * self.D_r * np.sin(self.C_r * np.arctan(self.B_r * alpha_r))
+        
+        Fx = self.C_m * throttle - self.Cr_0 - self.Cr_2 * vx * vx
+
+        rt = (np.tan(steering_angle)*vx)/(self.length_f + self.length_r)
+        Mtv = self.ptv * (rt - r)
+
+
+        model.set_rhs('s', sdot)
+        model.set_rhs('n', 
+            vx*np.sin(mu) + vy*np.cos(mu)
+        )
+        model.set_rhs('mu',
+            r - self.k(s)*sdot                    
+        )
+        model.set_rhs('vx',
+            (1.0 / self.mass) * (Fx - Fy_f * np.sin(steering_angle) + self.mass * vy * r)
+        )
+        model.set_rhs('vy', 
+            (1.0 / self.mass) * (Fy_r + Fy_f * np.cos(steering_angle) - self.mass * vx * r)
+        )
+        model.set_rhs('r',
+            (1.0 / self.rotational_inertia) * (Fy_f * self.length_f * np.cos(steering_angle) - Fy_r * self.length_r + Mtv)
+        )
+        model.set_rhs('throttle', throttle_change)
+        model.set_rhs('steering_angle', steering_angle_change)
+
+
+        return model
