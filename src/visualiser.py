871888eef1b00596c4f2dd5fbe2906ee20dd6e72
SpicyHolo <41269364+SpicyHolo@users.noreply.github.com>
2024-09-19 16:43:19 +0200

    add ploting track to visualiser.py, reading simulation data

--- src/visualiser.py
+++ src/visualiser.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import json
+import numpy as np
+from scipy.interpolate import splev, splprep
 
 from mpc.track import Track
 
@@ -9,9 +11,32 @@ class Visualiser:
         self.track = track
         
         self.fig, self.ax = plt.subplots(figsize=(16, 9))
+        #self.sim_data = self.read_sim_data(json_path)
+
 
     def plot_optimal_path(self):
-        path = self.track.optimal_path
-        x, y = path.get_sample_points()
-        self.ax.plot(x, y)
+        self.ax.plot(self.track.optimal_path.get_sample_points(), 'g')
+
+    def plot_track(self):
+        self.ax.plot(*self.track.left_bound.get_sample_points(), 'black')
+        self.ax.plot(*self.track.right_bound.get_sample_points(), 'black')
+
+    def read_sim_data(self, file_path):
+        sim_data = {}
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+            for key, value in data.items():
+                sim_data[key] = np.array(value)
+        return sim_data
+
+    def get_vehicle_positions(self, data):
+        n = len(data['x'])
+        positions = np.zeros(2, n)
+
+        # setup first position
+        positions[0] = self.track.optimal_path.position(s = 0.)
+
+        X = data['x']
+        for i,x in enumerate(X):
+            pass
 
